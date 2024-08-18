19bfcf6cac2b1cddd4f103a7298f84aabbfb78be
firelighted <asano.sar@gmail.com>
2024-08-18 16:35:00 -0700

    main menu add menu button at top of all scenes by having main menu be Main scene

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,12 +1,30 @@
 extends Node
 
+@onready var showhide_menu = $CanvasLayer/Control2
+@onready var menu_scene = $CanvasLayer
 
 func on_level_select_idx_pressed(level_idx:int):
 	print("Change to level %s" % level_idx)
 	# Remove the current level
-	get_child(0).visible = false
+	#get_child(0).visible = false
+	_on_toggle_main_menu_button_pressed() 
+	
+	for child in get_children():
+		if child != menu_scene:
+			child.queue_free() # delete other levels
+	
+	var next_level_resource
+	match level_idx:
+		0:
+			next_level_resource = preload("res://scenes/level0.tscn").instantiate()
+		1:
+			next_level_resource = preload("res://scenes/artonlylevel.tscn").instantiate()
+		1:
+			next_level_resource = preload("res://scenes/3dtestscene.tscn").instantiate()
+		_:
+			return
+	add_child(next_level_resource) #get_tree().get_root().
 
-	if level_idx == 0:
-		# Add the next level
-		var next_level_resource = preload("res://scenes/level0.tscn").instantiate()
-		get_tree().get_root().add_child(next_level_resource)
+
+func _on_toggle_main_menu_button_pressed() -> void:
+	showhide_menu.visible = !showhide_menu.visible
