85a15eb56c84c8bae4718507785f418edeb0708b
cousday <cousday@gmail.com>
2024-08-20 00:31:05 -0400

    level 2 fixes

--- scripts/level_complete_collectable.gd
+++ scripts/level_complete_collectable.gd
@@ -2,8 +2,8 @@ extends Area3D
 
 func _on_body_entered(_body: Node3D) -> void:
 	print("_on_body_entered")
-	$Explosion.explode()
-	$ExplosionSound.play()
+	$Explosion.explode()  
+	$LevelCompleteSound.play()
 	$Timer.start()
 
 func _on_timer_timeout() -> void:
